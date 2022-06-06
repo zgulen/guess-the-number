e6cb49b95f7178a9bd8fcfdf40169440fc9b9fde
zgulen <gulenzubeyir@gmail.com>
2022-06-06 18:32:05 +0300

    gettin better

--- app.js
+++ app.js
@@ -5,36 +5,44 @@ let inputData = document.getElementById("inputData")
 let info = document.getElementById("info")
 let firstNumber = document.getElementById("firstNumber")
 let secondNumber = document.getElementById("secondNumber")
+let life = 0
+let chance = 5
+info.innerHTML = `number of attempt: ${life} <br><br> You have only have ${chance} chance`
 function guessNumber() {
-    life = 1
+    chance--
+    life++
     let userInput = document.getElementById("input")
-    if (userInput.value > 100 || userInput.value < 0 || userInput.value == "") {
-        inputData.innerHTML = `Please enter a number that is lower 100 higher 0`
-    }
-    else if (number > userInput.value) {
-        inputData.innerHTML = userInput.value
-        userInput.value = ""
-        inputData.style.display = "none"
-        firstNumber.innerHTML = inputData.innerHTML
-        console.log(userInput.value);
-        info.innerHTML = `you have ${life++} left`
+    if (chance >= 0) {
+        if (chance == 0) {
+            updateText("You have Lost")
+        }
+        else if (userInput.value > 100 || userInput.value < 0 || userInput.value == "") {
+            inputData.innerHTML = `Please enter a number that is lower 100 higher 0`
+        }
+        else if (number > userInput.value) {
+            inputData.innerHTML = userInput.value
+            userInput.value = ""
+            inputData.style.display = "none"
+            firstNumber.innerHTML = inputData.innerHTML
+            info.innerHTML = `number of attempt: ${life}`
+            info.innerHTML = `number of attempt: ${life} <br><br> You have only have ${chance} chance`
+        } else if (number < userInput.value) {
+            inputData.innerHTML = userInput.value
+            userInput.value = ""
+            inputData.style.display = "none"
+            secondNumber.innerHTML = inputData.innerHTML
+            info.innerHTML = `number of attempt: ${life} <br><br> You have only have ${chance} chance`
 
-    } else if (number < userInput.value) {
-        inputData.innerHTML = userInput.value
-        userInput.value = ""
-        inputData.style.display = "none"
-        secondNumber.innerHTML = inputData.innerHTML
-        console.log(userInput.value);
-        info.innerHTML = `you have ${life++} left`
-
-    } else if (number == userInput.value) {
-        updateText("Congratulations, You are the Best");
+        } else if (number == userInput.value) {
+            updateText("Congratulations, You are the Best");
+        }
+    } else if (chance < 1) {
+        updateText("You have Lost")
     }
 
 }
-
 function updateText(text) {
-    let delay = 500;
+    let delay = 10;
     let h1 = document.querySelector(".header");
     h1.innerHTML = text
         .split("")
@@ -46,6 +54,7 @@ function updateText(text) {
     Array.from(h1.children).forEach((span, index) => {
         setTimeout(() => {
             span.classList.add("wavy");
-        }, index * 60 + delay);
+        }, index * 50 + delay);
     });
 }
+
