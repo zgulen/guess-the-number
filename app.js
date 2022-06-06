af0221f24bc360b7397a9b57ec1f0f63b60c423c
zgulen <gulenzubeyir@gmail.com>
2022-06-06 22:03:42 +0300

    hacky

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const number = Math.floor(Math.random() * 100) + 1;
-console.log(number);
 //! input box
 let inputData = document.getElementById("inputData")
 let info = document.getElementById("info")
@@ -9,22 +8,28 @@ let life = 0
 let chance = 5
 info.innerHTML = `number of attempt: ${life} <br><br> You have only have ${chance} chance`
 function guessNumber() {
+    // chance--
+    // life++
+    let userInput = document.getElementById("input")
     chance--
     life++
-    let userInput = document.getElementById("input")
     if (chance >= 0) {
+
         if (chance == 0) {
             updateText("You have Lost")
+            info.innerHTML = `number of attempt: ${life} <br><br> You dont have any chance`
         }
-        else if (userInput.value > 100 || userInput.value < 0 || userInput.value == "") {
+        if (userInput.value > 100 || userInput.value < 0 || userInput.value == "") {
             inputData.innerHTML = `Please enter a number that is lower 100 higher 0`
+            chance++
+            life--
         }
         else if (number > userInput.value) {
             inputData.innerHTML = userInput.value
             userInput.value = ""
             inputData.style.display = "none"
             firstNumber.innerHTML = inputData.innerHTML
-            info.innerHTML = `number of attempt: ${life}`
+            // info.innerHTML = `number of attempt: ${life}`
             info.innerHTML = `number of attempt: ${life} <br><br> You have only have ${chance} chance`
         } else if (number < userInput.value) {
             inputData.innerHTML = userInput.value
@@ -35,11 +40,9 @@ function guessNumber() {
 
         } else if (number == userInput.value) {
             updateText("Congratulations, You are the Best");
+            return
         }
-    } else if (chance < 1) {
-        updateText("You have Lost")
     }
-
 }
 function updateText(text) {
     let delay = 10;
